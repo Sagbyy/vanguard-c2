bb9c009ace5be7f7d8ba4945980a3973790150bb
Nisseya <yassinehadi78190@gmail.com>
2026-06-12 18:24:03 -0700

    fix: handled more cases platform

--- vanguard-platform/src/platform.rs
+++ vanguard-platform/src/platform.rs
@@ -188,6 +188,11 @@ impl Platform {
             }
 
             Message::StrategyUpdate { .. } => {}
+            Message::TrackUpdated { track } => {}
+            Message::InterceptorUpdate {
+                platform_id,
+                interceptor,
+            } => {}
         }
 
         Ok(())
@@ -280,6 +285,7 @@ mod tests {
                 id: Uuid::new_v4(),
                 position: Position { x: 0.0, y: 0.0 },
                 state: InterceptorState::Idle,
+                assigned_track: None,
             }],
             neighbor_platforms: vec![NeighborPlatform {
                 id: Uuid::new_v4(),
